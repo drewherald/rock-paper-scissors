43189aaeef5130013f85f743deffc097e8e40bfc
drewherald <drewherald9@gmail.com>
2022-09-26 15:32:46 -0400

    Created Game Logic in script.js
    Created the playRound function to implement the logic
    for the r/p/s game

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 function getComputerChoice(){
-    let value = Math.Random();
-    let choice;
+    let value = Math.random();
     if(value<0.33){
-        choice = "rock";
+        return "rock";
     } else if(value<0.66){
-        choice = "paper";
+        return "paper";
     } else {
-        choice = "scissors";
+        return "scissors";
     }
 }
+
+    function playRound(playerSelection, computerSelection){
+        if(playerSelection.toLowerCase === computerSelection){
+            return `Tie! Both players chose ${playerSelection}`;
+        } 
+
+        if(playerSelection === "rock"){
+            if(computerSelection === "paper"){
+                return "You Lose! Paper beats Rock!"
+            } else{
+                return "You Win! Rock beats Scissors!"
+            }
+        }
+
+        if(playerSelection === "paper"){
+            if(computerSelection === "scissors"){
+                return "You Lose! Scissors beats Paper!"
+            } else{
+                return "You Win! Paper beats Rock!"
+            }
+        }
+
+        if(playerSelection === "scissors"){
+            if(computerSelection === "rock"){
+                return "You Lose! Rock beats Scissors!"
+            } else{
+                return "You Win! Scissors beats Paper!"
+            }
+        }
+
+        return "error";
+    }
+
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+
+    
